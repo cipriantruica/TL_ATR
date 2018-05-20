f7332d26ca0a277c16ee7841e21051526d0014ad
Ciprian Truica <ciprian.octavian.truica@gmail.com>
2018-05-20 16:33:39 +0300

    run script

--- src/run.sh
+++ src/run.sh
@@ -9,7 +9,7 @@ DB2=NewsArticlesDB
 DB3=ArxivDB
 NUM_ITER=200
 NUM_TESTS=1
-python3.6 main.py $DB1 $NUM_ITER $NUM_TESTS >> $PATHR$DB1"_results"
-python3.6 main.py $DB2 $NUM_ITER $NUM_TESTS >> $PATHR$DB2"_results"
-python3.6 main.py $DB3 $NUM_ITER $NUM_TESTS >> $PATHR$DB3"_results"
+python3.6 main.py $DB1 $NUM_ITER $NUM_TESTS >> $PATHR$DB1"_results_new"
+#python3.6 main.py $DB2 $NUM_ITER $NUM_TESTS >> $PATHR$DB2"_results_new"
+python3.6 main.py $DB3 $NUM_ITER $NUM_TESTS >> $PATHR$DB3"_results_new"
 
